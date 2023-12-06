9e1c16f0dad5b63ebf74d0764b4ccfdccdf5688d
Daniel Villano <84256368+xDani-v@users.noreply.github.com>
2023-12-06 04:16:08 +0000

    cc1

--- reto1.js
+++ reto1.js
@@ -1,3 +1,7 @@
+const giftIds = [2, 1, 3, 5, 3, 2]
+const firstRepeatedId = findFirstRepeated(giftIds)
+console.log(firstRepeatedId) 
+
 function findFirstRepeated(gifts) {
   let aux = [];
   if(gifts.length==0) return -1;
